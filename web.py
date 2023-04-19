d86f219766b41696965c9684c7ecf29ccba0771a
SH-Tejasvi <113892581+SH-Tejasvi@users.noreply.github.com>
2023-04-19 14:54:11 +0530

    Update web.py

--- web.py
+++ web.py
@@ -5,3 +5,7 @@ def hello_world():
 	return 'Hello World'
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
+@app.route('/health')
+def health_check():
+    # simulate an error by returning a 500 Internal Server Error response code
+    return 'Internal Server Error', 500
